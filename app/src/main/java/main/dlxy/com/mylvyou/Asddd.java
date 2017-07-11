1b733d6f6143e5acaa271f7552d42f5c3b1893ed
T <503601228@qq.com>
2017-07-11 09:58:15 +0800

    大法官大法官大法官

--- app/src/main/java/main/dlxy/com/mylvyou/Asddd.java
+++ app/src/main/java/main/dlxy/com/mylvyou/Asddd.java
@@ -1,9 +1,18 @@
 package main.dlxy.com.mylvyou;
 
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
+
 /**
  * Created by T on 2017/7/11.
  */
 
-public class Asddd {
-
+public class Asddd extends Activity{
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+    }
 }
+
